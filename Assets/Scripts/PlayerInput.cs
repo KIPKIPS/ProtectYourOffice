23b20d5d6c2deb45e21ee7597cb975534b1f451c
RECNEPS-KIPS <2432719656@qq.com>
2020-02-01 11:40:23 +0800

    实现输入转换成二维信号值

--- Assets/Scripts/PlayerInput.cs
+++ Assets/Scripts/PlayerInput.cs
@@ -3,41 +3,24 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerInput : MonoBehaviour {
-    public Vector3 targetVector;//x值为左右移动,z值为前后移动
-    public bool move;//是否移动
-    public bool up;
-    public bool down;
-    public bool left;
-    public bool right;
+    public string keyUp = "w";
+    public string keyDown = "s";
+    public string keyLeft = "a";
+    public string keyRight = "d";
+
+    //输入信号值
+    public float Dup;
+    public float Dright;
     void Awake() {
 
     }
 
     void Start() {
-        down=left=right=up = false;
-        move = false;
+
     }
 
     void Update() {
-        up = Input.GetKey(KeyCode.W);
-        down = Input.GetKey(KeyCode.S);
-        left = Input.GetKey(KeyCode.A);
-        right = Input.GetKey(KeyCode.D);
-
-        targetVector = SquareToCircle((right ? 1 : 0) - (left ? 1 : 0), (up ? 1 : 0) - (down ? 1 : 0));
-        
-        move = up || down || left || right;
+        Dup = (Input.GetKey(keyUp) ? 1.0f : 0) - (Input.GetKey(keyDown) ? 1.0f : 0);
+        Dright = (Input.GetKey(keyRight) ? 1.0f : 0) - (Input.GetKey(keyLeft) ? 1.0f : 0);
     }
-
-    Vector3 SquareToCircle(float x, float z) {
-        Vector3 circleVector;
-        //球坐标z
-        circleVector.x= x * Mathf.Sqrt(1 - Mathf.Pow(z, 2) / 2);
-        //球坐标x
-        circleVector.z= z * Mathf.Sqrt(1 - Mathf.Pow(x, 2) / 2);
-
-        circleVector.y = 0;
-        return circleVector;
-    }
-
 }
