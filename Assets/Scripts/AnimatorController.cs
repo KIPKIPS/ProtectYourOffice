ed626289c76b0ac77f64f0b037b80f5a2cfb10d3
RECNEPS-KIPS <2432719656@qq.com>
2020-02-01 15:18:00 +0800

    实现行走

---
+++ Assets/Scripts/AnimatorController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimatorController : MonoBehaviour {
+    public GameObject model;
+    public PlayerInput pi;
+    [SerializeField]
+    public Animator animator;
+
+    public float walkSpeed = 2.0f;
+
+    public Rigidbody rigid;
+    private Vector3 movingVec;//玩家移动值
+
+    void Awake() {
+        //初始化
+        pi = GetComponent<PlayerInput>();
+        model = GameObject.Find("Model");
+        animator = model.GetComponent<Animator>();
+        rigid = GetComponent<Rigidbody>();
+    }
+    // Start is called before the first frame update
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+        //设置动画混合值
+        animator.SetFloat("forward", pi.Dmag);
+        if (pi.Dmag > 0.1f) {
+            Vector3 tempDir = Vector3.Slerp(model.transform.forward, pi.Dvec, 0.1f);//转向的平滑插值
+            Debug.DrawRay(model.transform.position, tempDir, Color.red, 200);
+            model.transform.forward = tempDir;
+        }
+        movingVec = pi.Dmag * model.transform.forward * walkSpeed;
+    }
+
+    void FixedUpdate() {
+        //rigid.position+=movingVec*Time.fixedDeltaTime;
+        rigid.velocity = movingVec;
+    }
+}
--- Assets/Scripts/PlayerInput.cs
+++ Assets/Scripts/PlayerInput.cs
@@ -19,6 +19,9 @@ public class PlayerInput : MonoBehaviour {
     private float velocityDright;
     //输入开关
     public bool inputEnable = true;
+
+    public float Dmag;//动画的forward值,代表玩家是否有输入,玩家输入量
+    public Vector3 Dvec;//目标朝向
     void Awake() {
 
     }
@@ -29,9 +32,17 @@ public class PlayerInput : MonoBehaviour {
 
     void Update() {
         //目标值
-        targetDup = inputEnable?(Input.GetKey(keyUp) ? 1.0f : 0) - (Input.GetKey(keyDown) ? 1.0f : 0):0;
-        targetDright = inputEnable?(Input.GetKey(keyRight) ? 1.0f : 0) - (Input.GetKey(keyLeft) ? 1.0f : 0):0;
+        targetDup = (Input.GetKey(keyUp) ? 1.0f : 0) - (Input.GetKey(keyDown) ? 1.0f : 0);
+        targetDright = (Input.GetKey(keyRight) ? 1.0f : 0) - (Input.GetKey(keyLeft) ? 1.0f : 0);
+        if (inputEnable==false) {
+            targetDup = 0;
+            targetDright = 0;
+        }
         Dup = Mathf.SmoothDamp(Dup, targetDup, ref velocityDup, 0.1f);
         Dright = Mathf.SmoothDamp(Dright, targetDright, ref velocityDright, 0.1f);
+
+        Dmag = Mathf.Sqrt(Dup * Dup + Dright * Dright);
+        Dvec =  Dright * transform.right+ Dup * transform.forward;
     }
+    
 }
