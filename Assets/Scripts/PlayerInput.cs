2b8ec73226d94a60dbd6d5ef5035c4d80edc9e52
RECNEPS-KIPS <2432719656@qq.com>
2020-02-01 12:51:58 +0800

    实现动画混合树

--- Assets/Scripts/PlayerInput.cs
+++ Assets/Scripts/PlayerInput.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -11,6 +12,13 @@ public class PlayerInput : MonoBehaviour {
     //输入信号值
     public float Dup;
     public float Dright;
+    //平滑输入值
+    private float targetDup;
+    private float targetDright;
+    private float velocityDup;
+    private float velocityDright;
+    //输入开关
+    public bool inputEnable = true;
     void Awake() {
 
     }
@@ -20,7 +28,10 @@ public class PlayerInput : MonoBehaviour {
     }
 
     void Update() {
-        Dup = (Input.GetKey(keyUp) ? 1.0f : 0) - (Input.GetKey(keyDown) ? 1.0f : 0);
-        Dright = (Input.GetKey(keyRight) ? 1.0f : 0) - (Input.GetKey(keyLeft) ? 1.0f : 0);
+        //目标值
+        targetDup = inputEnable?(Input.GetKey(keyUp) ? 1.0f : 0) - (Input.GetKey(keyDown) ? 1.0f : 0):0;
+        targetDright = inputEnable?(Input.GetKey(keyRight) ? 1.0f : 0) - (Input.GetKey(keyLeft) ? 1.0f : 0):0;
+        Dup = Mathf.SmoothDamp(Dup, targetDup, ref velocityDup, 0.1f);
+        Dright = Mathf.SmoothDamp(Dright, targetDright, ref velocityDright, 0.1f);
     }
 }
